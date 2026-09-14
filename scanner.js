0e4158a8b1c871f1827bb940eef0717383fd7784
purbalinggabmtc-cmyk <purbalinggabmtc@gmail.com>
2026-09-14 17:41:20 +0700

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -1,5 +1,5 @@
 const API_URL =
-"https://script.google.com/macros/s/XXXX/exec";
+"https://script.google.com/macros/s/AKfycbyCwRWw3LaA7-nDq7CLQ_uDZK2n275Ad-a0O3qZI3VjglHTbEuV9E32DvWJWZw2XjzZ0w/exec";
 
 
 
